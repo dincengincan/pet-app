6879aeec5e54f4e8911045bb75817996a41c8d25
dincengincan <engincandinc@gmail.com>
2019-12-24 00:42:29 +0300

    Updates

---
+++ precache-manifest.9510fd6ba5ce865dcbe0ed7dfab741c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac6ec4af9375906d0c76b2f64483c15",
+    "url": "/pets-app-react/index.html"
+  },
+  {
+    "revision": "1dbbf5800cbf80d1f3a4",
+    "url": "/pets-app-react/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "0159a47b2307f0def0ab",
+    "url": "/pets-app-react/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "1dbbf5800cbf80d1f3a4",
+    "url": "/pets-app-react/static/js/2.58564b1a.chunk.js"
+  },
+  {
+    "revision": "0159a47b2307f0def0ab",
+    "url": "/pets-app-react/static/js/main.86e1ed48.chunk.js"
+  },
+  {
+    "revision": "31c3b01019bc1f2ed0de",
+    "url": "/pets-app-react/static/js/runtime-main.aa2e716e.js"
+  }
+]);
